fe32485d127e2e16a0fa214cf0a1b70fe8140567
Hugo <hugo.almazanlaforet@gmail.com>
2026-09-16 11:43:05 +0200

    Initial commit

---
+++ src/client/java/fr/frifri/survieevolution/client/SurvieEvolutionClient.java
@@ -0,0 +1,10 @@
+package fr.frifri.survieevolution.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SurvieEvolutionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
